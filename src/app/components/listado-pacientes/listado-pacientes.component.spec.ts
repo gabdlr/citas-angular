257be37dddb57e059abc68ebac1449f269a417a8
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-11-18 21:34:13 -0300

    test(ut): listado pacientes component test update

--- src/app/components/listado-pacientes/listado-pacientes.component.spec.ts
+++ src/app/components/listado-pacientes/listado-pacientes.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListadoPacientesComponent } from './listado-pacientes.component';
+import { Paciente } from '../../models/paciente';
+import { FormsModule } from '@angular/forms';
 
 describe('ListadoPacientesComponent', () => {
   let component: ListadoPacientesComponent;
@@ -8,7 +10,8 @@ describe('ListadoPacientesComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ListadoPacientesComponent]
+      declarations: [ListadoPacientesComponent],
+      imports: [FormsModule],
     });
     fixture = TestBed.createComponent(ListadoPacientesComponent);
     component = fixture.componentInstance;
@@ -17,5 +20,18 @@ describe('ListadoPacientesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return patientId to trackBy', () => {
+    const mockPatient = new Paciente(
+      'mock@mock.com',
+      '2023-11-18',
+      'mockId',
+      'mockName',
+      'mockOwner',
+      'mockSymptoms'
+    );
+    const trackByReturnedId = component.trackBy(0, mockPatient);
+    expect(trackByReturnedId).toBe(mockPatient.id);
   });
 });
