f65f593f7b686445559bda4267766f65f6b2b190
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-11-18 21:24:24 -0300

    test(ut): paciente component test update

--- src/app/components/paciente/paciente.component.spec.ts
+++ src/app/components/paciente/paciente.component.spec.ts
@@ -1,21 +1,55 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PacienteComponent } from './paciente.component';
+import { PacientesService } from '../../services/pacientes.service';
+import { Paciente } from '../../models/paciente';
+import { By } from '@angular/platform-browser';
+import { BotonFormularioComponent } from '../boton-formulario/boton-formulario.component';
 
 describe('PacienteComponent', () => {
   let component: PacienteComponent;
   let fixture: ComponentFixture<PacienteComponent>;
-
+  let pacientesServiceMock: jasmine.SpyObj<PacientesService>;
+  let patientMock: Paciente;
   beforeEach(() => {
+    pacientesServiceMock = jasmine.createSpyObj(PacientesService, [
+      'editarPaciente',
+      'eliminarPaciente',
+    ]);
     TestBed.configureTestingModule({
-      declarations: [PacienteComponent]
+      declarations: [PacienteComponent, BotonFormularioComponent],
+      providers: [
+        { provide: PacientesService, useValue: pacientesServiceMock },
+      ],
     });
     fixture = TestBed.createComponent(PacienteComponent);
+    patientMock = new Paciente();
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call editarPacient on service when editarPaciente function is triggered', () => {
+    component.paciente = patientMock;
+    fixture.debugElement
+      .query(By.css("[data-testId='pacientCardEditButton']"))
+      .triggerEventHandler('click', {});
+
+    expect(pacientesServiceMock.editarPaciente).toHaveBeenCalledOnceWith(
+      patientMock
+    );
+  });
+
+  it('should call eliminarPaciente on service when eliminarPaciente function is triggered', () => {
+    component.paciente = patientMock;
+    fixture.debugElement
+      .query(By.css("[data-testId='pacientCardDeleteButton']"))
+      .triggerEventHandler('click', {});
+    expect(pacientesServiceMock.eliminarPaciente).toHaveBeenCalledOnceWith(
+      patientMock
+    );
   });
 });
